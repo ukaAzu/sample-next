a57d61c16a2ad0dc062d4a632b0a5af5711bbdde
ukaAzu <yukan1988@gmail.com>
2024-04-03 17:41:11 +0900

    p.127

--- src/app/other/page.tsx
+++ src/app/other/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <main>
             <h1 className="title">Other page</h1>
             <p className="msg">これは別のページです</p>
+            <div>
+                <Image src="/food_konbini_onigiri.png" width={200} height={200} />
+            </div>
             <div>
                 <Link href="/">Back</Link>
             </div>
