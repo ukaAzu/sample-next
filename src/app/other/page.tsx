c1b0c802ee833411c527d938ac558cf38cb6c0c0
ukaAzu <yukan1988@gmail.com>
2024-04-03 17:42:06 +0900

    alt属性追加

--- src/app/other/page.tsx
+++ src/app/other/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
             <h1 className="title">Other page</h1>
             <p className="msg">これは別のページです</p>
             <div>
-                <Image src="/food_konbini_onigiri.png" width={200} height={200} />
+                <Image src="/food_konbini_onigiri.png" width={200} height={200} alt="おにぎり" />
             </div>
             <div>
                 <Link href="/">Back</Link>
